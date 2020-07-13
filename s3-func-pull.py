b20c3d955adfd515dd1b2eb8c679abfe6f13cc91
Ross Lawrence <52179159+Lawreros@users.noreply.github.com>
2020-07-13 13:25:40 -0400

    Update s3-func-pull.py

--- s3-func-pull.py
+++ s3-func-pull.py
@@ -11,52 +11,58 @@ from math import factorial
 import numpy as np
 
 bucket = 'ndmg-data'
-path = 'IPCAS_6/IPCAS_6-m2g-func-04-15-20'
-localpath = '/IPCAS_6'
+paths = ['IPCAS4/Py3_test']
+localpaths = ['/IPCAS4'] #SWU4, BNU3
+
+num=paths.__len__()
 
 subj_pattern = r"(?<=sub-)(\w*)(?=/ses)"
 sesh_pattern = r"(?<=ses-)(\d*)"
 
-all_subfiles = get_matching_s3_objects(bucket, path + "/sub-", suffix="")
+for qq in range(num):
+    path=paths[qq]
+    localpath=localpaths[qq]
 
-subjs = list(set(re.findall(subj_pattern, obj)[0] for obj in all_subfiles))
+    all_subfiles = get_matching_s3_objects(bucket, path + "/sub-", suffix="")
 
-seshs = OrderedDict()
-client = m2g.utils.cloud_utils.s3_client(service="s3")
+    subjs = list(set(re.findall(subj_pattern, obj)[0] for obj in all_subfiles))
 
-for subj in subjs:
-    prefix = f"{path}/sub-{subj}/"
-    all_seshfiles = get_matching_s3_objects(bucket, prefix, "measure-correlation.csv")
-    seshs = list(obj for obj in all_seshfiles)
-    # sesh = list(set([re.findall(sesh_pattern, obj)[0] for obj in all_seshfiles]))
+    seshs = OrderedDict()
+    client = m2g.utils.cloud_utils.s3_client(service="s3")
 
-    # Load in all the files selected above for the given subject
-    for csv in seshs:
-        atlas = csv.split("/")[-3]
-        subsesh = f"sub-{csv.split('/')[-7]}"
+    for subj in subjs:
+        prefix = f"{path}/sub-{subj}/"
+        all_seshfiles = get_matching_s3_objects(bucket, prefix, "measure-correlation.csv")
+        seshs = list(obj for obj in all_seshfiles)
+        # sesh = list(set([re.findall(sesh_pattern, obj)[0] for obj in all_seshfiles]))
 
-        os.makedirs(f"{localpath}/{atlas}/RAW", exist_ok=True)
-        os.makedirs(f"{localpath}/{atlas}/NEW", exist_ok=True)
+        # Load in all the files selected above for the given subject
+        for csv in seshs:
+            atlas = csv.split("/")[-2]
+            subsesh = f"{csv.split('/')[-9]}_{csv.split('/')[-8]}"
 
-        client.download_file(bucket, f"{csv}", f"{localpath}/{atlas}/RAW/{subsesh}_measure-correlation.csv")
-        print(f"Downloaded {csv}")
+            os.makedirs(f"{localpath}/{atlas}/RAW", exist_ok=True)
+            os.makedirs(f"{localpath}/{atlas}/NEW", exist_ok=True)
 
-        my_data = genfromtxt(f"{localpath}/{atlas}/RAW/{subsesh}_measure-correlation.csv", delimiter=',', skip_header=1)
+            client.download_file(bucket, f"{csv}", f"{localpath}/{atlas}/RAW/{subsesh}_measure-correlation.csv")
+            print(f"Downloaded {csv}")
 
-        a = sum(range(1, len(my_data)))
-        arr = np.zeros((a,3))
-        z=0
-        for num in range(len(my_data)):
-            for i in range(len(my_data[num])):
-                if i > num:
-                    #print(f'{num+1} {i+1} {my_data[num][i]}')
-                    arr[z][0]= f'{num+1}'
-                    arr[z][1]= f'{i+1}'
-                    arr[z][2] = my_data[num][i]
-                    z=z+1
-            
-        np.savetxt(f"{localpath}/{atlas}/NEW/{subsesh}_measure-correlation.csv", arr,fmt='%d %d %f', delimiter=' ')
-        print(f"Converted")
+            my_data = genfromtxt(f"{localpath}/{atlas}/RAW/{subsesh}_measure-correlation.csv", delimiter=',', skip_header=1)
+
+            a = sum(range(1, len(my_data)))
+            arr = np.zeros((a,3))
+            z=0
+            for num in range(len(my_data)):
+                for i in range(len(my_data[num])):
+                    if i > num:
+                        #print(f'{num+1} {i+1} {my_data[num][i]}')
+                        arr[z][0]= f'{num+1}'
+                        arr[z][1]= f'{i+1}'
+                        arr[z][2] = my_data[num][i]
+                        z=z+1
+                
+            np.savetxt(f"{localpath}/{atlas}/NEW/{subsesh}_measure-correlation.csv", arr,fmt='%d %d %f', delimiter=' ')
+            print(f"Converted")
 
 
 
